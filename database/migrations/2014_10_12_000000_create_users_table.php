53355e67465b05a1ba0d4d055bef9f0a89655617
Ridlwan Yunus <hiddenjustice309@gmail.com>
2016-07-12 12:45:47 +0700

    1 Commit | Post Request Fix

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $field) {
+            $field->increments('id');
+            $field->unsignedInteger('roles_id')->nullable();
+            $field->string('username')->unique();;
+            $field->string('name');
+            $field->string('email')->unique();
+            $field->string('password');
+            $field->rememberToken();
+            $field->timestamps();
+        });
+
+        Schema::create('roles', function (Blueprint $field) {
+            $field->increments('id');
+            $field->string('namaRule');
+        });
+
+        Schema::table('users', function (Blueprint $field) {
+            $field  ->foreign('roles_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+        Schema::drop('roles');
+    }
+}
---
+++ resources/views/register/formRegister.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Register</title>
+
+    <!-- Bootstrap core CSS -->
+     <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form action="{{ url(action('RegisterController@postRegister')) }}" method="post" class="form-signin">
+        <h2 class="form-signin-heading">Please sign up</h2>
+
+        <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputEmail" class="sr-only">Username</label>
+        <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputEmail" class="sr-only">Name</label>
+        <input type="text" name="name" class="form-control" placeholder="Name" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+        <label for="inputPassword" class="sr-only">Re-Password</label>
+        <input type="password" name="repassword" class="form-control" placeholder="Re-Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
